d7d7791069d2cd234caddaa3e56bb8704caa9103
Oluwabamidele Ayanda <oluwabamideleayanda@gmail.com>
2023-09-18 17:27:58 +0100

    updated app

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,6 @@
 def registry = 'https://oluwadevops.jfrog.io/'
 def imageName = 'oluwadevops.jfrog.io/oluwadevops-docker-local/ttrend'
-def version   = '2.1.2'
+def version   = '2.1.3'
 pipeline {
     agent {
         node {
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-kubectl apply -f deployment.yml
+
 kubectl apply -f namespace.yml
+kubectl apply -f deployment.yml
 kubectl apply -f service.yml
 kubectl apply -f secret.yml
-kubectl apply -f secret.yml
+kubectl apply -f secret.yml 
